8b3c08ea4c348c62f11a0dba9d386d75699399d1
jhonestiago <jonestiago.94@gmail.com>
2025-01-21 20:12:31 -0300

    update

--- src/pages/Main.py
+++ src/pages/Main.py
@@ -2,6 +2,7 @@ import sys
 
 from gui_principal import Ui_MainWindow
 from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtGui import QIcon
 
 class Main(Ui_MainWindow, QMainWindow):
 
@@ -9,6 +10,8 @@ class Main(Ui_MainWindow, QMainWindow):
         super().__init__(parent)
         super().setupUi(self)
         self.__init_components()
+        self.__error_color = 'background-color: rgb(204, 41, 54); color: rgb(255, 255, 255);'
+        self.__sucess_color = 'background-color: rgb(101, 184, 145);'
 
     def __init_components(self) -> None:
         #pageLogin
@@ -17,8 +20,28 @@ class Main(Ui_MainWindow, QMainWindow):
         self.pushButtonLoginRegister.clicked.connect(self.__pageRegistration)
         self.pushButtonLoginCloseMessage.clicked.connect(self.frameLoginMessages.hide)
 
+        #pageSystem
+        self.pushButtonSystemForm.setIcon(QIcon('src/img/form.png'))
+        self.pushButtonSystemDatabase.setIcon(QIcon('src/img/database.png'))
+        self.pushButtonSystemUsers.setIcon(QIcon('src/img/user.png'))
+        self.pushButtonSystemSettings.setIcon(QIcon('src/img/settings.png'))
+
+        #.pageForm
+
     def __enter(self) -> None:
-        pass
+        username = self.lineEditLoginUsername.text()
+        password = self.lineEditLoginPassword.text()
+        if username == "admin" and  password == "12345":
+            self.lineEditLoginUsername.clear()
+            self.lineEditLoginPassword.clear()
+            self.frameLoginMessages.hide()
+            self.__pageForm()
+        else:
+            message = 'Usuário ou Senha incorretos'
+            self.labelLoginMessage.setText(message)
+            self.labelLoginMessage.setStyleSheet(self.__error_color)
+            self.frameLoginMessages.show()
+
 
     def __pageLogin(self) -> None:
         self.stackedWidgetMain.setCurrentWidget(self.pageLogin)
