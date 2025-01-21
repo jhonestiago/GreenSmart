885df1e56f0161e49f081008aada8cf376233efd
jhonestiago <jonestiago.94@gmail.com>
2025-01-21 20:47:27 -0300

    update

--- src/models/Cliente.py
+++ src/models/Cliente.py
@@ -1,4 +1,9 @@
 class Cliente:
 
     def __init__(self):
-        pass
+        self.__name:str = ''
+        self.__cpf_cnpj:str = ''
+        self.__address:str = ''
+        self.__email:str = ''
+        self.__phone_number:str = ''
+        self.__error_message:str = ''
