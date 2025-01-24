b23e62113198b393b83e377a1d1c4f00df9b3cab
jhonestiago <jonestiago.94@gmail.com>
2025-01-24 18:46:03 -0300

    update

--- src/controller/UserControl.py
+++ src/controller/UserControl.py
@@ -6,7 +6,10 @@ class UserControl:
     def __init__(self) -> None:
         self.__users:List[User] = []
 
-    def add_user(self, user:User) -> None:
-        message = 'Usuário adicionado com sucesso'
+    def add_user(self, user:User) -> str:
         self.__users.append(user)
-        return message
+        return 'Usuário adicionado com sucesso'
+
+    def remove_user(self, index:int) -> str:
+        del self.__users[index]
+        return 'Usuário removido com sucesso'
